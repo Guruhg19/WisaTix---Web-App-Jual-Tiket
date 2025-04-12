a157a50694bbd571b8276f6e94725be5d530cd47
Guruhg19 <guruhvitra19@gmail.com>
2025-04-12 14:43:21 +0700

    Add SellerRepository and SellerRepositoryInterface; update AppServiceProvider and FrontService to integrate seller data retrieval

--- app/Services/FrontService.php
+++ app/Services/FrontService.php
@@ -3,30 +3,34 @@
 namespace App\Services;
 
 use App\Repositories\Contracts\CategoryRepositoryInterface;
+use App\Repositories\Contracts\SellerRepositoryInterface;
 use App\Repositories\Contracts\TicketRepositoryInterface;
 
 class FrontService
 {
     protected $categoryRepository;
+    protected $sellerRepository;
     protected $ticketRepository;
 
-    public function __construct(TicketRepositoryInterface $ticketRepository, CategoryRepositoryInterface $categoryRepository)
+    public function __construct(TicketRepositoryInterface $ticketRepository, CategoryRepositoryInterface $categoryRepository, SellerRepositoryInterface $sellerRepository)
     {
         $this->ticketRepository = $ticketRepository;
         $this->categoryRepository = $categoryRepository;
+        $this->sellerRepository = $sellerRepository;
     }
 
     // Get Front Data
     public function getFrontData()
     {
         $categories = $this->categoryRepository->getAllCategories();
+        $sellers = $this->sellerRepository->getAllSellers();
         $popularTickets = $this->ticketRepository->getPopularTickets(4);
         $newTickets = $this->ticketRepository->getAllNewTickets();
 
-        return compact('categories', 'popularTickets', 'newTickets');
+        return compact('categories', 'popularTickets', 'newTickets', 'sellers');
     }
 
-    
+
 
 
 }
