fcd8de05032444e7b689a7e91fa7f1e68505c3f3
Guruhg19 <guruhvitra19@gmail.com>
2025-04-12 13:34:45 +0700

    Implement FrontController with service pattern and add TicketRepository for ticket management

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -6,7 +6,27 @@ use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
-    public function index(){
-        return view('front.index');
+    protected $frontService;
+
+    public function __construct(FrontService $frontService)
+    {
+        $this->frontService = $frontService;
     }
+
+    // Konsep Service Repository Pattern
+    public function index()
+    {
+        $data = $this->frontService->getFrontPageData();
+
+        return view('welcome', compact('data'));
+    }
+    
+
+    // Konsep MVC
+    // public function index(){
+    //     $categories = Category::latest()->get();
+    //     $popuparTickets = Ticket::where('is_popular', true)->latest()->take(10)->get();
+    //     $newTickets = Ticket::latest()->take(10)->get();
+    //     return view('welcome', compact('categories', 'popuparTickets', 'newTickets'));
+    // }
 }
